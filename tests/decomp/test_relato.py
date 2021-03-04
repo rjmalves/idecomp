002ce572f950b93df2edcc1f17d80d3d26cc2528
Rogerio Alves <rogerioalves.ee@gmail.com>
2021-03-04 11:28:04 -0300

    Testando leitura do relato de rv1 a rv4

--- tests/decomp/test_relato.py
+++ tests/decomp/test_relato.py
@@ -20,3 +20,39 @@ def test_eq_relato():
     leitor2 = LeituraRelato("tests/_arquivos")
     leitor2.le_arquivo()
     assert leitor2.relato == leitor.relato
+
+
+def test_leitura_rv1():
+    leitor2 = LeituraRelato("tests/_arquivos")
+    leitor2.le_arquivo("relato.rv1")
+    n = leitor2.relato.dados_gerais.numero_semanas_1_mes
+    assert n == 4
+    tabela_cmo = leitor2.relato.cmo.tabela
+    assert tabela_cmo.shape[1] == 4
+
+
+def test_leitura_rv2():
+    leitor2 = LeituraRelato("tests/_arquivos")
+    leitor2.le_arquivo("relato.rv2")
+    n = leitor2.relato.dados_gerais.numero_semanas_1_mes
+    assert n == 3
+    tabela_cmo = leitor2.relato.cmo.tabela
+    assert tabela_cmo.shape[1] == 3
+
+
+def test_leitura_rv3():
+    leitor2 = LeituraRelato("tests/_arquivos")
+    leitor2.le_arquivo("relato.rv3")
+    n = leitor2.relato.dados_gerais.numero_semanas_1_mes
+    assert n == 2
+    tabela_cmo = leitor2.relato.cmo.tabela
+    assert tabela_cmo.shape[1] == 2
+
+
+def test_leitura_rv4():
+    leitor2 = LeituraRelato("tests/_arquivos")
+    leitor2.le_arquivo("relato.rv4")
+    n = leitor2.relato.dados_gerais.numero_semanas_1_mes
+    assert n == 1
+    tabela_cmo = leitor2.relato.cmo.tabela
+    assert tabela_cmo.shape[1] == 1
