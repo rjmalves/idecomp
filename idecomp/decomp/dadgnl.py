949e722e9f7471191cca87933d40dd9c8296dec2
Rogerio Alves <rogerioalves.ee@gmail.com>
2022-07-26 09:35:24 -0300

    refactor dadgnl

--- idecomp/decomp/dadgnl.py
+++ idecomp/decomp/dadgnl.py
@@ -1,6 +1,7 @@
 from cfinterface.files.registerfile import RegisterFile
+from cfinterface.components.register import Register
 from idecomp.decomp.modelos.dadgnl import TG, GS, NL, GL
-from typing import Type, List, Optional, TypeVar, Any
+from typing import Type, List, Optional, TypeVar, Union
 
 
 class DadGNL(RegisterFile):
@@ -45,81 +46,191 @@ class DadGNL(RegisterFile):
         """
         return [b for b in self.data.of_type(registro)]
 
-    def __obtem_registro_com_codigo(
-        self, tipo: Type[T], codigo: int
-    ) -> Optional[T]:
-        regs: List[Any] = self.__registros_por_tipo(tipo)
-        for r in regs:
-            if r.codigo == codigo:
-                return r
-        return None
-
-    def __obtem_registro_do_estagio(
-        self, tipo: Type[T], codigo: int, estagio: int
-    ) -> Optional[T]:
-        regs: List[Any] = self.__registros_por_tipo(tipo)
-        for r in regs:
-            if all([r.codigo == codigo, r.estagio == estagio]):
-                return r
-        return None
+    def __obtem_registro(self, tipo: Type[T]) -> Optional[T]:
+        """ """
+        r = self.__obtem_registros(tipo)
+        return r[0] if len(r) > 0 else None
 
     def __obtem_registros(self, tipo: Type[T]) -> List[T]:
         return self.__registros_por_tipo(tipo)
 
-    def tg(self, codigo: int, estagio: int) -> Optional[TG]:
+    def __obtem_registros_com_filtros(
+        self, tipo_registro: Type[T], **kwargs
+    ) -> Optional[Union[T, List[T]]]:
+        def __atende(r) -> bool:
+            condicoes: List[bool] = []
+            for k, v in kwargs.items():
+                if v is not None:
+                    condicoes.append(getattr(r, k) == v)
+            return all(condicoes)
+
+        regs_filtro = [
+            r for r in self.__obtem_registros(tipo_registro) if __atende(r)
+        ]
+        if len(regs_filtro) == 0:
+            return None
+        elif len(regs_filtro) == 1:
+            return regs_filtro[0]
+        else:
+            return regs_filtro
+
+    def cria_registro(self, anterior: Register, registro: Register):
+        """
+        Adiciona um registro ao arquivo após um outro registro previamente
+        existente.
+
+        Este método existe para retrocompatibilidade e deve ser substituído
+        quando for suportado na classe :class:`RegisterFile`.
+        """
+        self.data.add_after(anterior, registro)
+
+    def deleta_registro(self, registro: Register):
+        """
+        Remove um registro existente no arquivo.
+
+        Este método existe para retrocompatibilidade e deve ser substituído
+        quando for suportado na classe :class:`RegisterFile`.
+        """
+        self.data.remove(registro)
+
+    def lista_registros(self, tipo: Type[T]) -> List[T]:
+        """
+        Lista todos os registros presentes no arquivo que tenham o tipo `T`.
+
+        Este método existe para retrocompatibilidade e deve ser substituído
+        quando for suportado na classe :class:`RegisterFile`.
+        """
+        return [r for r in self.data.of_type(tipo)]
+
+    def append_registro(self, registro: Register):
+        """
+        Adiciona um registro ao arquivo na última posição.
+
+
+        Este método existe para retrocompatibilidade e deve ser substituído
+        quando for suportado na classe :class:`RegisterFile`.
+        """
+        self.data.append(registro)
+
+    def preppend_registro(self, registro: Register):
+        """
+        Adiciona um registro ao arquivo na primeira posição.
+
+        Este método existe para retrocompatibilidade e deve ser substituído
+        quando for suportado na classe :class:`RegisterFile`.
+        """
+        self.data.preppend(registro)
+
+    def tg(
+        self,
+        codigo: Optional[int] = None,
+        subsistema: Optional[int] = None,
+        nome: Optional[str] = None,
+        estagio: Optional[int] = None,
+    ) -> Optional[Union[TG, List[TG]]]:
         """
         Obtém um registro que define uma usina termelétrica existente
         no estudo descrito pelo :class:`DadGNL`.
 
         :param codigo: código que especifica o registro
             da UTE
-        :type codigo: int
+        :type codigo: int | None
+        :param subsistema: código que especifica o subsistema
+            da UTE
+        :type subsistema: int | None
+        :param nome: nome da UTE
+        :type nome: str | None
         :param estagio: Índice do estágio para o qual foi cadastrado
             o despacho da UTE
-        :type estagio: int
-        :return: Um registro do tipo :class:`TG`
+        :type estagio: int | None
+        :return: Um ou mais registros, se existirem.
+        :rtype: :class:`TG` | list[:class:`TG`] | None
         """
-        return self.__obtem_registro_do_estagio(TG, codigo, estagio)
+        return self.__obtem_registros_com_filtros(
+            TG,
+            codigo=codigo,
+            subsistema=subsistema,
+            nome=nome,
+            estagio=estagio,
+        )
 
-    def gs(self, mes: int) -> Optional[GS]:
+    def gs(
+        self, mes: Optional[int] = None, semanas: Optional[int] = None
+    ) -> Optional[Union[GS, List[GS]]]:
         """
         Obtém um registro que define o número de semanas em cada
         mês de estudo no :class:`DadGNL`.
 
         :param mes: índice do mês no estudo
-        :type mes: int
+        :type mes: int | None
+        :param semanas: número de semanas do mês
+        :type semanas: int | None
 
-        :return: Um registro do tipo :class:`GS`
+        :return: Um ou mais registros, se existirem.
+        :rtype: :class:`GS` | list[:class:`GS`] | None
         """
-        regs: List[GS] = self.__obtem_registros(GS)
-        for r in regs:
-            if r.mes == mes:
-                return r
-        return None
+        return self.__obtem_registros_com_filtros(
+            GS,
+            mes=mes,
+            semanas=semanas,
+        )
 
-    def nl(self, codigo: int) -> Optional[NL]:
+    def nl(
+        self,
+        codigo: Optional[int] = None,
+        subsistema: Optional[int] = None,
+        lag: Optional[int] = None,
+    ) -> Optional[Union[NL, List[NL]]]:
         """
         Obtém um registro que define o número de lags para o despacho
         de uma UTE.
 
         :param codigo: código da UTE
-        :type codigo: int
+        :type codigo: int | None
+        :param subsistema: subsistema da UTE
+        :type subsistema: int | None
+        :param lag: número de lags da UTE
+        :type lag: int | None
 
-        :return: Um registro do tipo :class:`NL`
+        :return: Um ou mais registros, se existirem.
+        :rtype: :class:`NL` | list[:class:`NL`] | None
         """
-        return self.__obtem_registro_com_codigo(NL, codigo)
+        return self.__obtem_registros_com_filtros(
+            NL,
+            codigo=codigo,
+            subsistema=subsistema,
+            lag=lag,
+        )
 
-    def gl(self, codigo: int, estagio: int) -> Optional[GL]:
+    def gl(
+        self,
+        codigo: Optional[int] = None,
+        subsistema: Optional[int] = None,
+        estagio: Optional[int] = None,
+        data_inicio: Optional[str] = None,
+    ) -> Optional[Union[GL, List[GL]]]:
         """
         Obtém um registro que define o despacho por patamar
         e a duração dos patamares para uma UTE GNL.
 
         :param codigo: código que especifica o registro
             da UTE
-        :type codigo: int
-        :param estagio: Índice do estágio para o qual foi cadastrado
-            o despacho da UTE
-        :type estagio: int
-        :return: Um registro do tipo :class:`GL`
+        :type codigo: int | None
+        :param subsistema: subsistema da UTE
+        :type subsistema: int | None
+        :param estagio: estágio do despacho da UTE
+        :type estagio: int | None
+        :param data_inicio: data de início do estágio
+            do despacho da UTE
+        :type data_inicio: str | None
+
+        :return: Um ou mais registros, se existirem.
+        :rtype: :class:`GL` | list[:class:`GL`] | None
         """
-        return self.__obtem_registro_do_estagio(GL, codigo, estagio)
+        return self.__obtem_registros_com_filtros(
+            GL,
+            codigo=codigo,
+            subsistema=subsistema,
+            estagio=estagio,
+            data_inicio=data_inicio,
+        )
