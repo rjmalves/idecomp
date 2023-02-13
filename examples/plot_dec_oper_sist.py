f6d7a175b358a9688b2461ec12fe1af291283e62
Rogerio Alves <rogerioalves.ee@gmail.com>
2023-02-12 23:13:33 -0300

    fix exemplo

--- examples/plot_dec_oper_sist.py
+++ examples/plot_dec_oper_sist.py
@@ -4,7 +4,7 @@ dec_oper_sist.csv
 ========================================
 """
 
-#%%
+# %%
 # O primeiro passo para realizar o processamento do arquivo, assim como os
 # demais arquivos .csv, é a leitura. A função de leitura recebe dois argumentos,
 # sendo que o segundo é opcional. Tendo lido o arquivo, é possível recuperar
@@ -15,14 +15,14 @@ arq = DecOperSist.le_arquivo(".")
 print(arq.versao)
 
 
-#%%
+# %%
 # A tabela de informações é fornecida como um dataframe, onde os nomes das colunas geralmente
 # remetem aos nomes originais das colunas do arquivo .csv, porém em camelCase.
 df = arq.tabela
 print(df.columns)
 
 
-#%%
+# %%
 # A partir deste dataframe é possível realizar análises e produzir visualizações. Por exemplo,
 # utilizando o módulo plotly. Deste ponto em diante, não é mais necessário o conhecimento
 # específico do arquivo ou da idecomp.
@@ -32,7 +32,7 @@ estagio_mensal = df["periodo"].max()
 # sphinx_gallery_thumbnail_number = 1
 fig = px.box(
     df.loc[df["periodo"] == estagio_mensal],
-    x="nomeSubmercado",
-    y="geracaoTermicaMW",
+    x="nome_submercado",
+    y="geracao_termica_MW",
 )
 fig
