81ac2451469b6274473bb5f37a5b7221cdc7f411
Rogerio Alves <rogerioalves.ee@gmail.com>
2023-06-30 17:45:51 -0300

    implementa custos e testes

---
+++ idecomp/decomp/custos.py
@@ -0,0 +1,120 @@
+from idecomp.decomp.modelos.custos import BlocoRelatorioCustos
+
+from cfinterface.components.block import Block
+from cfinterface.files.blockfile import BlockFile
+from typing import List, TypeVar, Optional
+import pandas as pd  # type: ignore
+
+# Para compatibilidade - até versão 1.0.0
+from os.path import join
+import warnings
+
+
+class Custos(BlockFile):
+    """
+    Armazena os dados de saída do DECOMP referentes aos
+    custos associados à solução do problema.
+
+    Esta classe lida com as informações de saída do
+    DECOMP reproduzidas no `custos.rvx`.
+
+    """
+
+    T = TypeVar("T")
+
+    BLOCKS = [
+        BlocoRelatorioCustos,
+    ]
+
+    def __init__(self, data=...) -> None:
+        super().__init__(data)
+        self.__relatorios_variaveis_duais = None
+        self.__relatorios_fcf = None
+
+    @classmethod
+    def le_arquivo(cls, diretorio: str, nome_arquivo="custos.rv0") -> "Custos":
+        msg = (
+            "O método le_arquivo(diretorio, nome_arquivo) será descontinuado"
+            + " na versão 1.0.0 - use o método read(caminho_arquivo)"
+        )
+        warnings.warn(msg, category=FutureWarning)
+        return cls.read(join(diretorio, nome_arquivo))
+
+    def escreve_arquivo(self, diretorio: str, nome_arquivo="custos.rv0"):
+        msg = (
+            "O método escreve_arquivo(diretorio, nome_arquivo) será"
+            + " descontinuado na versão 1.0.0 -"
+            + " use o método write(caminho_arquivo)"
+        )
+        warnings.warn(msg, category=FutureWarning)
+        self.write(join(diretorio, nome_arquivo))
+
+    def __concatena_blocos_por_tipo(
+        self, blocos, indice_data: int
+    ) -> Optional[pd.DataFrame]:
+        """
+        Adiciona uma coluna com o estágio de cada bloco, assumindo
+        a mesma ordem das séries de energia.
+        :param blocos: Os blocos a serem concatenados
+        :type bloco: List[Type[T]]
+        :return: O DataFrame com os estágios
+        :rtype: pd.DataFrame
+        """
+        df = None
+        for b in blocos:
+            if not isinstance(b, Block):
+                continue
+            df_estagio = b.data[indice_data]
+            if df is None:
+                df = df_estagio
+            else:
+                df = pd.concat([df, df_estagio], ignore_index=True)
+        if df is not None:
+            return df
+        return None
+
+    @property
+    def relatorio_variaveis_duais(self) -> Optional[pd.DataFrame]:
+        """
+        Obtém a tabela das variáveis duais do armazenamento de cada
+        UHE, nos problemas resolvidos pelo DECOMP.
+
+        - estagio (`int`)
+        - cenario (`int`)
+        - usina (`str`)
+        - pih (`float`)
+
+        :return: O DataFrame com os valores
+        :rtype: pd.DataFrame | None
+        """
+        if self.__relatorios_variaveis_duais is None:
+            blocos_custos: List[BlocoRelatorioCustos] = []
+            for b in self.data.of_type(BlocoRelatorioCustos):
+                blocos_custos.append(b)
+            self.__relatorios_variaveis_duais = (
+                self.__concatena_blocos_por_tipo(blocos_custos, 0)
+            )
+        return self.__relatorios_variaveis_duais
+
+    @property
+    def relatorio_fcf(self) -> Optional[pd.DataFrame]:
+        """
+        Obtém a tabela dos cortes da FCF nos quais o DECOMP acoplou durante
+        a resolução dos seus problemas.
+
+        - estagio (`int`)
+        - cenario (`int`)
+        - indice_corte (`int`)
+        - parcela_pi (`float`)
+
+        :return: O DataFrame com os valores
+        :rtype: pd.DataFrame | None
+        """
+        if self.__relatorios_fcf is None:
+            blocos_custos: List[BlocoRelatorioCustos] = []
+            for b in self.data.of_type(BlocoRelatorioCustos):
+                blocos_custos.append(b)
+            self.__relatorios_fcf = self.__concatena_blocos_por_tipo(
+                blocos_custos, 1
+            )
+        return self.__relatorios_fcf
---
+++ idecomp/decomp/modelos/custos.py
@@ -0,0 +1,143 @@
+# Imports do próprio módulo
+
+# Imports de módulos externos
+from cfinterface.components.block import Block
+from cfinterface.components.line import Line
+from cfinterface.components.integerfield import IntegerField
+from cfinterface.components.literalfield import LiteralField
+from cfinterface.components.floatfield import FloatField
+import pandas as pd  # type: ignore
+from typing import IO, List
+
+
+class BlocoRelatorioCustos(Block):
+    """ """
+
+    BEGIN_PATTERN = r" RELATORIO DAS VARIAVEIS DUAIS NO ESTAGIO"
+    END_PATTERN = "X----X-"
+
+    def __init__(self, previous=None, next=None, data=None) -> None:
+        super().__init__(previous, next, data)
+        self.__scenario_line = Line([IntegerField(2, 44), IntegerField(3, 55)])
+        self.__dual_variables_line = Line(
+            [
+                LiteralField(14, 4),
+                FloatField(15, 19, 2),
+            ]
+        )
+        self.__fcf_line = Line(
+            [
+                IntegerField(4, 4),
+                FloatField(12, 9, 3),
+            ]
+        )
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, BlocoRelatorioCustos):
+            return False
+        bloco: BlocoRelatorioCustos = o
+        if not all(
+            [
+                isinstance(self.data, list),
+                isinstance(bloco.data, list),
+            ]
+        ):
+            return False
+        else:
+            return all(
+                [
+                    self.data[0].equals(bloco.data[0]),
+                    self.data[1].equals(bloco.data[1]),
+                ]
+            )
+
+    def __read_bloco_variaveis_duais(self, file: IO):
+        def converte_tabela_para_df() -> pd.DataFrame:
+            cols = [
+                "usina",
+                "pih",
+            ]
+            df = pd.DataFrame(data={"usina": usinas, "pih": pihs})
+            cols_adic = [
+                "estagio",
+                "cenario",
+            ]
+            df["estagio"] = [estagio] * len(usinas)
+            df["cenario"] = [cenario] * len(usinas)
+            df = df[cols_adic + cols]
+            return df
+
+        estagio: int = self.dados_cenario[0]
+        cenario: int = self.dados_cenario[1]
+
+        # Salta 3 linhas
+        for _ in range(3):
+            file.readline()
+
+        # Variáveis auxiliares
+        usinas: List[str] = []
+        pihs: List[float] = []
+        while True:
+            linha: str = file.readline()
+            # Verifica se acabou
+            if "X---------------X" in linha:
+                self.data.append(converte_tabela_para_df())
+                break
+            dados = self.__dual_variables_line.read(linha)
+            usinas.append(dados[0])
+            pihs.append(dados[1])
+
+    def __read_bloco_restricoes_fcf(self, file: IO):
+        def converte_tabela_para_df() -> pd.DataFrame:
+            cols = [
+                "indice_corte",
+                "parcela_pi",
+            ]
+            df = pd.DataFrame(
+                data={"indice_corte": indices, "parcela_pi": pis}
+            )
+            cols_adic = [
+                "estagio",
+                "cenario",
+            ]
+            df["estagio"] = [estagio] * len(indices)
+            df["cenario"] = [cenario] * len(indices)
+            df = df[cols_adic + cols]
+            return df
+
+        estagio: int = self.dados_cenario[0]
+        cenario: int = self.dados_cenario[1]
+
+        # Salta 3 linhas
+        for _ in range(3):
+            file.readline()
+
+        # Variáveis auxiliares
+        indices: List[int] = []
+        pis: List[float] = []
+        while True:
+            linha: str = file.readline()
+            # Verifica se acabou
+            if len(linha) < 3:
+                self.data.append(converte_tabela_para_df())
+                break
+            dados = self.__fcf_line.read(linha)
+            indices.append(dados[0])
+            pis.append(dados[1])
+
+    # Override
+    def read(self, file: IO, *args, **kwargs):
+        str_bloco_variaveis_duais = "Aproveitamento  Bal.Hidr."
+        str_bloco_restricoes_fcf = "Restricoes da FCF"
+
+        # Extrai os dados de estágio e cenário
+        self.dados_cenario = self.__scenario_line.read(file.readline())
+
+        self.data: List[pd.DataFrame] = []
+        while True:
+            linha = file.readline()
+            if str_bloco_variaveis_duais in linha:
+                self.__read_bloco_variaveis_duais(file)
+            elif str_bloco_restricoes_fcf in linha:
+                self.__read_bloco_restricoes_fcf(file)
+                break
