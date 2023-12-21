811aa0fd43b4018e73752f822dacc39d420526a2
Rogerio <rogerioalves.ee@gmail.com>
2023-12-21 11:50:09 -0300

    substitui map por applymap (compat py 3.8)

--- idecomp/decomp/dadgnl.py
+++ idecomp/decomp/dadgnl.py
@@ -27,7 +27,7 @@ class Dadgnl(RegisterFile):
         super().__init__(data)
 
     def __expande_colunas_df(self, df: pd.DataFrame) -> pd.DataFrame:
-        colunas_com_listas = df.map(
+        colunas_com_listas = df.applymap(
             lambda linha: isinstance(linha, list)
         ).all()
         nomes_colunas = [
