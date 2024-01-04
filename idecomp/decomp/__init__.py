05b4ebc21426a0eb6c417cfe3604a19914a13e73
Rogerio <rogerioalves.ee@gmail.com>
2024-01-04 13:51:44 -0300

    atualiza versão e changelog

--- idecomp/decomp/__init__.py
+++ idecomp/decomp/__init__.py
@@ -2,10 +2,12 @@
 
 from .arquivos import Arquivos  # noqa
 from .caso import Caso  # noqa
+from .custos import Custos  # noqa
 from .dadger import Dadger  # noqa
 from .dadgnl import Dadgnl  # noqa
 from .dec_avl_evap import DecAvlEvap  # noqa
 from .dec_cortes_evap import DecCortesEvap  # noqa
+from .dec_desvfpha import DecDesvFpha  # noqa
 from .dec_eco_cotajus import DecEcoCotajus  # noqa
 from .dec_eco_discr import DecEcoDiscr  # noqa
 from .dec_oper_gnl import DecOperGnl  # noqa
@@ -15,7 +17,6 @@ from .dec_oper_sist import DecOperSist  # noqa
 from .dec_oper_usie import DecOperUsie  # noqa
 from .dec_oper_usih import DecOperUsih  # noqa
 from .dec_oper_usit import DecOperUsit  # noqa
-from .custos import Custos  # noqa
 from .decomptim import Decomptim  # noqa
 from .fcfnw import Fcfnw  # noqa
 from .hidr import Hidr  # noqa
