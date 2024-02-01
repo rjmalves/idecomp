9242bda94b898095271da483ccb7c6ce501ce3cb
marianasnoel <marianasimoesnoel@gmail.com>
2024-02-01 15:28:17 -0300

    implementa classes para arquivos de saída libs

--- idecomp/decomp/modelos/blocos/versaomodelo.py
+++ idecomp/decomp/modelos/blocos/versaomodelo.py
@@ -26,3 +26,26 @@ class VersaoModelo(Block):
         linha = file.readline()
         modelo_linha = Line([LiteralField(size=7, starting_position=29)])
         self.data = modelo_linha.read(linha)[0]
+
+
+class VersaoModeloLibs(Block):
+    """
+    Bloco para ler a versão utilizada do modelo a partir da linha de
+    título do arquivo.
+    """
+
+    BEGIN_PATTERN = r"Nome do Modelo: DECOMP Versão:"
+    END_PATTERN = ""
+
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, VersaoModeloLibs):
+            return False
+        else:
+            if not all([type(self.data) is str, type(o.data) is str]):
+                return False
+            return self.data == o.data
+
+    def read(self, file: IO, *args, **kwargs):
+        linha = file.readline()
+        modelo_linha = Line([LiteralField(size=6, starting_position=32)])
+        self.data = modelo_linha.read(linha)[0]
